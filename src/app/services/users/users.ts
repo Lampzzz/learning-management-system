3dd7873992a6529f97239a26ed99cd763eef04ac
Lampz <lampazaj@gmail.com>
2025-10-29 21:19:33 +0800

    update create method and add index and delete method

--- src/app/services/users/users.ts
+++ src/app/services/users/users.ts
@@ -9,6 +9,16 @@ export class UsersService {
   private endpoint = 'http://localhost:3000/users';
 
   create(payload: User) {
-    return this.http.post(this.endpoint, payload);
+    const userId = Date.now().toString();
+    const created_at = new Date();
+    return this.http.post(this.endpoint, { userId, ...payload, created_at });
+  }
+
+  index() {
+    return this.http.get<User[]>(this.endpoint);
+  }
+
+  delete(id: string) {
+    return this.http.delete<User[]>(this.endpoint + '/' + id);
   }
 }
