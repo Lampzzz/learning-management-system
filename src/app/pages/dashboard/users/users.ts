082ce09d646fc7f9a04b32afd96c140bf9fd1343
Lampz <lampazaj@gmail.com>
2025-10-31 21:52:01 +0800

    ref: remove create user modal

--- src/app/pages/dashboard/users/users.ts
+++ src/app/pages/dashboard/users/users.ts
@@ -1,26 +1,20 @@
-import { Component, inject, signal } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, inject, signal, ViewChild } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { UsersService } from '../../../services/users/users';
 import { DashboardHeader } from '../../../shared/components/dashboard-header/dashboard-header';
-import { Modal } from '../../../shared/components/modal/modal';
+import { UserEditor } from '../../../shared/users/editor/editor';
 
 @Component({
   selector: 'app-users',
-  imports: [DashboardHeader, Modal, ReactiveFormsModule],
+  imports: [DashboardHeader, ReactiveFormsModule, UserEditor],
   templateUrl: './users.html',
   styleUrl: './users.scss',
 })
 export class Users {
+  @ViewChild('user_editor') userEditor!: UserEditor;
   private usersService = inject(UsersService);
 
   data = signal<User[] | null>(null);
-
-  createUserForm = new FormGroup({
-    username: new FormControl('', [Validators.required]),
-    email: new FormControl('@gmail.com', [Validators.required]),
-    password: new FormControl('password', [Validators.required]),
-    role: new FormControl('student', [Validators.required]),
-  });
 
   ngOnInit() {
     this.getUsers();
@@ -35,17 +29,14 @@ export class Users {
     });
   }
 
-  handleSubmit() {
-    this.usersService.create(this.createUserForm.value as User).subscribe({
-      next: () => this.createUserForm.reset(),
-      error: (error) => console.log('Error: ', error),
-    });
-  }
-
   deleteUser(id: string) {
     this.usersService.delete(id).subscribe({
       next: () => console.log('success delete'),
       error: (error) => console.log('Error: ', error),
     });
+  }
+
+  setUserData(user: User) {
+    this.userEditor.setUserData(user);
   }
 }
