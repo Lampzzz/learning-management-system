e8771635c3eea052813cf7a30663308a31f5e29b
Lampz <lampazaj@gmail.com>
2025-12-04 21:04:54 +0800

    chore: remove unused code

--- lms-backend/routes/web.php
+++ lms-backend/routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\CourseController;
-use App\Models\Course;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/greeting', function () {
     return "hello world";
 });
-
-Route::post('/courses', [CourseController::class, 'store']);
