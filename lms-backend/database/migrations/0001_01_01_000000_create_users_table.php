5dbf05ca726ef1d13982fae0824d330829449ce1
Lampz <lampazaj@gmail.com>
2025-12-04 21:05:58 +0800

    chore: update user schema

--- lms-backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ lms-backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,12 +12,11 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->id();
+            $table->id()->unique();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['student', 'instructor']);
             $table->timestamps();
         });
 
