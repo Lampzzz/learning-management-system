c82124de203f4807217417ff2956fd4281a3c43a
Lampz <lampazaj@gmail.com>
2025-12-09 22:07:46 +0800

    feat: add register and login endpoint

--- lms-backend/routes/api.php
+++ lms-backend/routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/users', [UserController::class, 'store']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
